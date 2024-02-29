b122d315579ddc1cdffd72befb1445442ce599b3
Radoslav Draganov <r.draganov02@gmail.com>
2024-02-29 22:54:02 +0200

    group/channel join/leave + create channel functions added

--- collab-messenger/src/services/group-services.ts
+++ collab-messenger/src/services/group-services.ts
@@ -1,4 +1,4 @@
-import { get, ref, query, orderByChild, push } from "firebase/database";
+import { get, ref, query, orderByChild, push, set, remove } from "firebase/database";
 import { db } from "../config/firebase-setup";
 
 export const addGroup = async (owner: string, name: string) => {
@@ -62,4 +62,42 @@ export const getGroupByName = async (name: string) => {
     const desiredGroup = allGroups.filter(group => group.name === name);
     
     return desiredGroup;
+};
+
+export const joinGroup = async (groupId: string, userHandle: string) => {
+    const userRef = ref(db, `groups/${groupId}/members/${userHandle}`);
+    await set(userRef, true);
+};
+
+export const leaveGroup = async (groupId: string, userHandle: string) => {
+    const userRef = ref(db, `groups/${groupId}/members/${userHandle}`);
+    await remove(userRef);
+};
+
+
+//group channel functions
+
+export const addGroupChannel = async (groupId: string, name: string, publicity: boolean, owner: string) => {
+ 
+    const channel = {
+        name,
+        createdOn: Date.now(),
+        members: {},
+        publicity
+    }
+
+    channel.members[owner] = true;
+
+    return push(ref(db, `groups/${groupId}/channels`), channel);
+
+}
+
+export const joinGroupChannel = async (groupId: string, userHandle: string, channelId: string) => {
+    const userRef = ref(db, `groups/${groupId}/channels/${channelId}/members/${userHandle}`);
+    await set(userRef, true);
+};
+
+export const leaveGroupChannel = async (groupId: string, userHandle: string, channelId: string) => {
+    const userRef = ref(db, `groups/${groupId}/channels/${channelId}/members/${userHandle}`);
+    await remove(userRef);
 };
