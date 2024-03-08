09355fa16e35ad2d29966201cb43d5aaa546c246
Radoslav Draganov <r.draganov02@gmail.com>
2024-03-08 23:31:08 +0200

    SingleChannelText better visual view

--- collab-messenger/src/components/Channel Components/SIngleChannelText/SingleChannelText.tsx
+++ collab-messenger/src/components/Channel Components/SIngleChannelText/SingleChannelText.tsx
@@ -1,9 +1,12 @@
+import { Box, Text, Flex } from "@chakra-ui/react";
 
-export default function SingleChannelText ({message}) {
+export default function SingleChannelText({ message }) {
     return (
-        <>
-        <p>{message.userHandle}:</p>
-        <span>{message.text}</span>
-        </>
-    )
+        <Flex align="center">
+            <Text fontWeight="bold" marginRight="2">{message.userHandle}:</Text>
+            <Box mt={2} border="1px" borderRadius="50" p="2">
+                <Text>{message.text}</Text>
+            </Box>
+        </Flex>
+    );
 }
