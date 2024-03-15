24d5db877569326ffa8df93855648e1da4fb3d76
Radoslav Draganov <r.draganov02@gmail.com>
2024-03-15 18:33:10 +0200

    SingleChannelText visual design

--- collab-messenger/src/components/Channel Components/SIngleChannelText/SingleChannelText.tsx
+++ collab-messenger/src/components/Channel Components/SIngleChannelText/SingleChannelText.tsx
@@ -4,8 +4,8 @@ export default function SingleChannelText({ message }) {
     return (
         <Flex align="center">
             <Text fontWeight="bold" marginRight="2">{message.userHandle}:</Text>
-            <Box mt={2} border="1px" borderRadius="50" p="2">
-                <Text>{message.text}</Text>
+            <Box mt={2} border="1px" borderRadius="50" p="2" pr="4" pl="4" backgroundColor="navy" color="white" whiteSpace="pre-wrap" textAlign="left">
+                <Text fontWeight="bold">{message.text}</Text>
             </Box>
         </Flex>
     );
